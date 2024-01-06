c03498f49e3014d7fe0fbdd84f23e5325e9002c6
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-01-06 22:55:25 +0100

    desolidarized path module from enemies

---
+++ Find Mocha/Assets/PathController.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    private readonly float Time_before_instantiation = .5f;
+    private readonly float precision = .2f;
+
+    [Header("Options")]
+    [SerializeField]
+    private float startSpeed;
+    [SerializeField]
+    private float totalTime;
+    [SerializeField, Tooltip("If true, uses totalTime as reference for one cycle and ignores startSpeed.\nIf false, uses startSpeed and ignores totalTime.")]
+    private bool useTotalTime;
+    [SerializeField]
+    private bool activateOnAwake = true;
+    [SerializeField, Range(0f,1f)]
+    private float phaseNormalized = 0;
+
+    [Header("Path")]
+    [SerializeField]
+    private float endLag;
+    [SerializeField]
+    private Vector2 startPath;
+    [SerializeField]
+    private Vector2 endPath;
+
+    private Rigidbody2D rb;
+
+    private Vector2 startingPosition;
+    private Vector2 pointA;
+    private Vector2 pointB;
+    private bool activated;
+    private bool started;
+    private float speed;
+    private float phaseTime;
+
+
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        Vector2 pos;
+
+        if (!started)
+            pos = transform.position;
+        else
+            pos = startingPosition;
+
+        pointB = pos + endPath;
+        pointA = pos + startPath;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(pointA, pointB);
+    }
+
+#endif
+
+    public void Activate()
+    {
+        if (activated)
+            return;
+
+        activated = true;
+        StartCoroutine(PathProceed());
+    }
+
+    public void Deactivate() => activated = false;
+
+    public void SetSpeed(float speed) {
+        if(useTotalTime)
+        {
+            Debug.LogWarning("A script tries to modify the speed of the PathController script but it's using totalTime cycle.");
+            return;
+        }
+        this.speed = speed;
+        ComputePhase();
+    }
+
+    private void Awake()
+    {
+        TryGetComponent(out rb);
+
+        started = true;
+        startingPosition = transform.position;
+
+        pointB = (Vector2)transform.position + endPath;
+        pointA = (Vector2)transform.position + startPath;
+
+        speed = startSpeed;
+
+        if(useTotalTime)
+            speed = GetSpeedFromTotalTime();
+
+        ComputePhase();
+
+        if (activateOnAwake)
+        {
+            activated = true;
+            StartCoroutine(PathProceed());
+        }
+        else
+            activated = false;
+    }
+
+    private IEnumerator PathProceed()
+    {
+        yield return new WaitForSeconds(Time_before_instantiation);
+
+        yield return new WaitForSeconds(phaseTime);
+
+        while(activated)
+        {
+            rb.velocity = new Vector2(pointB.x - transform.position.x, pointB.y - transform.position.y).normalized * speed;
+
+
+            if (Mathf.Abs(pointB.x - transform.position.x) < precision && Mathf.Abs(pointB.y - transform.position.y) < precision)
+            {
+                (pointA, pointB) = (pointB, pointA);
+                yield return new WaitForSeconds(endLag);
+            }
+
+            yield return null;
+        }
+    }
+
+    private void ComputePhase()
+    {
+        if (useTotalTime)
+            phaseTime = totalTime * phaseNormalized;
+        else
+            phaseTime = speed == 0 ? 0 : (2 * (endPath - startPath).magnitude / speed + 2 * endLag) * phaseNormalized;
+    }
+
+    private float GetSpeedFromTotalTime()
+    {
+        if(totalTime - 2 * endLag <= 0)
+        {
+            Debug.LogError("The path of "+gameObject+" has an end lag that is too large for the totalTime.\nSpeed value has to be unexpected.");
+            return totalTime <= 0 ? 0 : 1/totalTime;
+        }
+
+        return (endPath-startPath).magnitude/(totalTime - 2 * endLag)/2;
+    }
+}
--- Find Mocha/Assets/Scripts/EnemyController.cs
+++ Find Mocha/Assets/Scripts/EnemyController.cs
@@ -7,104 +7,46 @@ using static UnityEngine.GraphicsBuffer;
 public class EnemyController : MonoBehaviour, IDamageble
 {
     [Header("Main properties")]
-    public float maxHp;
-    public float attack;
-    public float speed;
+    [SerializeField]
+    private float maxHp;
+    [SerializeField]
+    private float attack;
+    [SerializeField]
+    private float speed;
 
     [Header("Knockback")]
-    public float knockbackAngle;
-    public float knockbackForce;
-
-    [Header("Path")]
-    public float endLag;
-    public Vector2 startPath;
-    public Vector2 endPath;
-    public float precision = .2f;
+    [SerializeField]
+    private float knockbackAngle;
+    [SerializeField]
+    private float knockbackForce;
 
     private GameObject player;
     private PlayerController playerController;
 
     private Rigidbody2D rb;
     private Collider2D col;
-
-
-    private bool started = false;
-    private Vector2 startingPosition;
-    private Vector2 pointA;
-    private Vector2 pointB;
-
+    private PathController pathController;
 
     private bool isDead;
-    private bool isSuperdead;
-
     private float hp;
 
-
-#if UNITY_EDITOR
-
-    private void OnValidate()
-    {
-        Vector2 pos;
-
-        if(!started)
-            pos = transform.position;
-        else
-            pos = startingPosition;
-
-        pointB =  pos + endPath;
-        pointA = pos + startPath;
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(pointA, pointB);
-    }
-
-#endif
 
     private void Awake()
     {
         player = GameObject.Find("Milk");
         player.TryGetComponent(out playerController);
 
+        TryGetComponent(out pathController);
         TryGetComponent(out rb);
         TryGetComponent(out col);
-
-        startingPosition = transform.position;
-        started = true;
-
-        pointB = (Vector2)transform.position + endPath;
-        pointA = (Vector2)transform.position + startPath;
-
-        StartCoroutine(PathProceed());
     }
 
     private void Start()
     {
         hp = maxHp;
+        pathController.SetSpeed(speed);
+        pathController.Activate();
     }
-
-
-    private IEnumerator PathProceed()
-    {
-        while (true)
-        {
-            rb.velocity = new Vector2(pointB.x - transform.position.x, pointB.y - transform.position.y).normalized * speed;
-
-
-            if ( Mathf.Abs(pointB.x - transform.position.x) < precision && Mathf.Abs(pointB.y - transform.position.y) < precision)
-            {
-                (pointA, pointB) = (pointB, pointA);
-                yield return new WaitForSeconds(endLag);
-            }
-
-            yield return null;
-
-            if(isDead || isSuperdead)
-                yield break;
-        }
-    }
-
 
 
     public void Heal(float heal)
@@ -119,18 +61,16 @@ public class EnemyController : MonoBehaviour, IDamageble
     }
     public void InstaKill(IDamageble from, Vector2? knockback, float knockbackAngle = 0, float knockbackForce = 0)
     {
-        isSuperdead = true;
+        pathController.Deactivate();
         isDead = true;
         Destroy(rb);
         Destroy(col);
-        StartCoroutine(SuperDeath(knockback));
+        StartCoroutine(SuperDeathAnimation(knockback));
     }
 
-    private IEnumerator SuperDeath(Vector2? knockback)
+    private IEnumerator SuperDeathAnimation(Vector2? knockback)
     {
         //rb.bodyType = RigidbodyType2D.Static;
-
-        //Vector3 direction = (Vector2.up * Random.Range(-1f, 1f) + Vector2.right * Random.Range(-1f, 1f)).normalized;
 
         Vector3 direction = (Vector2)knockback;
 
@@ -144,10 +84,6 @@ public class EnemyController : MonoBehaviour, IDamageble
 
         while(timer < 5f)
         {
-            //transform.rotation = Quaternion.Euler(0, 0, timer * 720);
-
-            //transform.position += 20 * Time.deltaTime * direction.normalized;
-
             transform.SetPositionAndRotation(20 * Time.deltaTime * direction.normalized, Quaternion.Euler(0, 0, timer * 720));
 
             timer += Time.deltaTime;
@@ -163,13 +99,12 @@ public class EnemyController : MonoBehaviour, IDamageble
     private void CheckAlive()
     {
         if(hp <= 0)
-        {
             Death();
-        }
     }
 
     private void Death()
     {
+        pathController.Deactivate();
         isDead = true;
         Destroy(gameObject);
     }
